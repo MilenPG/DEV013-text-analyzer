802cccd481bc1130c6d6cdd9efb4d5a1f582d6a3
Milen <millalen.pg@gmail.com>
2023-12-15 17:05:00 -0300

    Reset button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import analyzer from './analyzer.js';
 console.log(analyzer)
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const reload = document.getElementById("reset-button").addEventListener("click", function() {
+location.reload();
+});
