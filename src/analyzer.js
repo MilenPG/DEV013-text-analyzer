445b4a8fc0432a3997eff1d06efd63be557650bc
Milen <millalen.pg@gmail.com>
2023-12-14 22:49:47 -0300

    Adición de imágen. Relleno cualquiera de funciones en JS para poder correr los test.

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,20 +1,20 @@
 const analyzer = {  
-  getWordCount: (text) => {
+  getWordCount: (text) => {console.log(text)
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCount: (text) => {
+  getCharacterCount: (text) => {console.log(text)
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCountExcludingSpaces: (text) => {
+  getCharacterCountExcludingSpaces: (text) => {console.log(text)
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => { console.log(text)   
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberCount: (text) => {
+  getNumberCount: (text) => {console.log(text)
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberSum: (text) => {
+  getNumberSum: (text) => {console.log(text)
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import analyzer from './analyzer.js';
-
+console.log(analyzer)
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
