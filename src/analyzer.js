3f4f7f994822ae6f4ab804e7db3bc1e30cda6a1f
Milen <millalen.pg@gmail.com>
2024-01-03 23:39:25 -0300

    Avance métodos y manipulación del DOM: word count, character count y character no spaces count.

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,45 @@
 const analyzer = {  
-  getWordCount: (text) => {console.log(text)
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+  getWordCount: (text) => {
+    const wordCount = text.split(" ").length;
+    return wordCount;
+    //por qué me cuenta los espacios como palabras? q hago para que no pase eso?
   },
-  getCharacterCount: (text) => {console.log(text)
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+  getCharacterCount: (text) => {
+    return text.length;
+    //const characterCount = text.length;
+    //return characterCount;
   },
-  getCharacterCountExcludingSpaces: (text) => {console.log(text)
+
+  getCharacterCountExcludingSpaces: (text) => {
+    const characterCountExcludingSpaces = text.replace(/\s/g, '').length;
+    return characterCountExcludingSpaces;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => { console.log(text)   
+
+  getAverageWordLength: (text) => { 
+       
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberCount: (text) => {console.log(text)
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    /*0. Establecer qué es un número (usar regular exp): numeros separados  NaN para ej: 45hjl62)
+      1. Recorrer string      
+      2. Excluir todo lo que no sea un número 
+      3. Contar lo que sí: if....text.length? o text.algo.length?
+      */
   },
+
   getNumberSum: (text) => {console.log(text)
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    /*0. Establecer qué es un número (usar regular exp, NaN para ej: 45hjl62)
+      1. Recorrer string
+      2. Identificar y almacenar solo los elementos que su valor sea un numero
+      3. Almacenar eltos en un array?
+      4. Retornar suma de todos los eltos del array creado.
+      */  
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import analyzer from './analyzer.js';
-console.log(analyzer)
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
+document.querySelector("textarea").addEventListener("input", function(event) {
+  const textArea = event.target.value;
+  /*ESTE SERÁ EL ARGUMENTO DE MI PARAMETRO (analyzer.getCharacterCount(texto);)
+  AHORA: 
+  - Ir a cada elto. de lista y reemplazar el título (MODIFICAR ¿textContent o innerText?) "Recuento de X", por: `Recuento de X\b${funcionrespectiva}`  */
+
+  const callWordCount = analyzer.getWordCount(textArea);
+  //console.log(`Recuento de palabras: ${callWordCount}`);
+  document.querySelector('li[data-testid="word-count"]').innerText = `Recuento de palabras:\n${callWordCount}`;
+
+  const callCharacterCount = analyzer.getCharacterCount(textArea);
+  //console.log("Recuento caracteres: " + callCharacterCount);
+  document.querySelector('li[data-testid="character-count"]').innerText = `Recuento caracteres:\n${callCharacterCount}`;
+  /*const showCharacterCount = document.querySelector('li[data-testid="character-count"]');
+  showCharacterCount.innerText = `Recuento caracteres \n \n ${callCharacterCount}`
+
+    ? const showCharacterCount = document.querySelector('li[data-testid="character-count"]');
+    ? showCharacterCount.innerHTML = `Recuento caracteres\b${characterCount}`;*/
+
+  const callCharacterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(textArea);
+  //console.log(`Recuento de caracteres sin espacio: ${callCharacterCountExcludingSpaces}`);
+  document.querySelector('li[data-testid="character-no-spaces-count"]').innerText = `Recuento de caracteres sin espacio:\n${callCharacterCountExcludingSpaces}`;
+
+});
+
+
 const reload = document.getElementById("reset-button").addEventListener("click", function() {
-location.reload();
+  location.reload();
 });
