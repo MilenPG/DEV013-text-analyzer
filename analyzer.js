bdd97bc4b04c0c69ae412c1a3287b7819bdeecbd
Milen <millalen.pg@gmail.com>
2024-01-09 14:01:39 -0300

    Updates

--- analyzer.js
+++ analyzer.js
@@ -1,22 +1,97 @@
 const analyzer = {  
-  getWordCount: (text) => {console.log(text)
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+  getWordCount: (text) => {
+    /*const wordCount = text.trim().replace(/[.,/#«»¡!$%^&*;:{}[\]=\-_`~()”“"…¿?\s]/g, '').split(/\s+/g).length;
+    return wordCount;*/
+    const word = text.trim().replace(/[.,/#«»¡!$%^&*;:{}[\]=\-_`~()”“"'"…¿?]/g, '').split(/\s+/g);
+    let wordCount = 0;
+    for (let i=0; i<word.length; i++){
+      if (word[i] !== '') {
+        wordCount = word.length;
+      }
+    }
+    return wordCount;
   },
-  getCharacterCount: (text) => {console.log(text)
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+  getCharacterCount: (text) => {
+    return text.length;
   },
-  getCharacterCountExcludingSpaces: (text) => {console.log(text)
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+  getCharacterCountExcludingSpaces: (text) => {
+    const characterCountExcludingSpaces = text.replace(/[.,/#«»¡!$%^&*;:{}[\]=\-_`~()”“"…¿?\s]/g, '').length;
+    return characterCountExcludingSpaces;
   },
-  getAverageWordLength: (text) => { console.log(text)   
+
+  getAverageWordLength: (text) => { 
+    const wordsCharacters = text.replace(/[/#«»¡!$%^&*;:{}[\]=\-_`~()”“"…¿?\s]/g, '').trim().length
+    const words = text.trim().split(/\s+/g)
+    const textsWords = words.length
+    const avarageWordLength = parseFloat((wordsCharacters/textsWords).toFixed(2))
+    return avarageWordLength;
+
+    /*0. Definir qué es una palabra
+      1. Contar caracteres de cada palabra (elto. del array)
+      2. Sacar promedio (dividir suma de todos los caracteres, por el numero de palabras) */       
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberCount: (text) => {console.log(text)
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberSum: (text) => {console.log(text)
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
-};
 
+  getNumberCount: (text) => {
+    const textArray = text.trim().split(/\s+/g)
+    /*const numberCount = textArray.filter(callback((/^[0-9]+$/)textArray[i])).length
+    return numberCount;*/
+    let numberCount = 0;
+    for (let i=0; i <textArray.length; i++) {
+      if (textArray[i] !== '') {
+        const number = Number(textArray[i]);
+        if (!isNaN(number)) {
+          numberCount++
+        }
+      }
+    }
+    return numberCount;
+
+    //const numberArray = textArray.replace(/^\w+/g, "")
+    //const numberArray = textArray.filter(onlyNumber);
+    //(function(i) {
+    
+    //if (textArray[i](/^[0-9]+$/)) {
+    //  numberCount = textArray((/^[0-9]+$/)[i]).length;
+      
+    //textArray(i) = (/^[0-9]+$/); {
+    //   return numberArray;
+    /*0. Establecer qué es un número (usar regular exp?): numeros separados por, NaN para ej: 45hjl62)
+      1. Recorrer string      
+      2. Tomar cada elemento que coincida con ser un numero y empujarlo a un array. (con if...)(array.filter)(match regex)
+      3. Retornar cantidad de elementos del array creado. (array.length)
+    */
+  },
+
+  getNumberSum: (text) => {
+    //const textArray = text.match(/\b\d+(\.\d+)?\b/g);
+    const textArray = text.split(/\s+/g);
+    let numberSum = 0;
+    for (let i=0; i<textArray.length; i++) {
+      //if ((!isNaN(textArray[i])) && textArray[i].replace(' ', '')) {
+      //numberSum += parseFloat(textArray[i]); 
+      //console.log(textArray[i]);
+      const number = Number(textArray[i]) //para "1.2 jaja 2.5."" no me cuenta el "2.5."
+      //console.log(number);
+      if (!isNaN(number)) {
+        numberSum+= number;
+        
+        /*numberSum = parseFloat((textArray).reduce(function (result, element){
+        return result+element;
+        }, 0)); */
+      }
+    }
+    return numberSum;
+  },
+    
+  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  /*0. Establecer qué es un número (usar regular exp?, o for con isNaN para ej: 45hjl62)
+      1. Recorrer string
+      2. Identificar y almacenar solo los elementos que su valor sea un numero
+      3. Almacenar eltos en un array?
+      4. Retornar suma de todos los eltos del array creado.*/
+};
 export default analyzer;
--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 import analyzer from './analyzer.js';
-console.log(analyzer)
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
+document.querySelector("textarea").addEventListener("input", function(event) {
+  const textArea = event.target.value;
+  /*ESTE SERÁ EL ARGUMENTO DE MI PARAMETRO (analyzer.getCharacterCount(texto);)
+  AHORA: 
+  - Ir a cada elto. de lista y reemplazar el título (MODIFICAR ¿textContent o innerText?) "Recuento de X", por: `Recuento de X\b${funcionrespectiva}`  */
+
+  const callWordCount = analyzer.getWordCount(textArea);
+  //console.log(`Recuento de palabras: ${callWordCount}`);
+  document.querySelector('li[data-testid="word-count"]').innerHTML = `Recuento de palabras:<br>${callWordCount}`;
+
+  const callCharacterCount = analyzer.getCharacterCount(textArea);
+  //console.log("Recuento caracteres: " + callCharacterCount);
+  document.querySelector('li[data-testid="character-count"]').innerHTML = `Recuento caracteres:<br>${callCharacterCount}`;
+  /*const showCharacterCount = document.querySelector('li[data-testid="character-count"]');
+  showCharacterCount.innerText = `Recuento caracteres \n \n ${callCharacterCount}`
+
+    ? const showCharacterCount = document.querySelector('li[data-testid="character-count"]');
+    ? showCharacterCount.innerHTML = `Recuento caracteres\b${characterCount}`;*/
+
+  const callCharacterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(textArea);
+  //console.log(`Recuento de caracteres sin espacio: ${callCharacterCountExcludingSpaces}`);
+  document.querySelector('li[data-testid="character-no-spaces-count"]').innerHTML = `Recuento de caracteres sin espacios, ni signos:<br>${callCharacterCountExcludingSpaces}`;
+
+  const callAvarageWordLength = analyzer.getAverageWordLength(textArea);
+  document.querySelector('li[data-testid="word-length-average"]').innerHTML = `Longitud promedio de palabras:<br>${callAvarageWordLength}`;
+
+  const callNumberCount = analyzer.getNumberCount(textArea);
+  //console.log(`Recuento de números: ${callNumberCount}`);
+  document.querySelector('li[data-testid="number-count"]').innerHTML = `Recuento de números:<br>${callNumberCount}`;
+
+  const callNumberSum = analyzer.getNumberSum(textArea);
+  //console.log(`Suma total de números: ${callNumberSum}`);
+  document.querySelector('li[data-testid="number-sum"]').innerHTML = `Suma total de números:<br>${callNumberSum}`;
+
+});
+
+
 const reload = document.getElementById("reset-button").addEventListener("click", function() {
-location.reload();
+  location.reload();
+  reload;
 });
