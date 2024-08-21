70d7506258522d5ba5120ef1b9bd2683268a06a6
Milen <millalen.pg@gmail.com>
2024-08-20 22:41:27 -0400

    fix: responsive UI check and reset button now only clean containers instead reload all

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 import analyzer from './analyzer.js';
 
+const wordCounter = document.querySelector('li[data-testid="word-count"]');
+const characterCounter = document.querySelector('li[data-testid="character-count"]');
+const characterNoSpacesCounter = document.querySelector('li[data-testid="character-no-spaces-count"]');
+const wordLengthAverageCounter = document.querySelector('li[data-testid="word-length-average"]');
+const numberCounter = document.querySelector('li[data-testid="number-count"]');
+const numberSumCounter = document.querySelector('li[data-testid="number-sum"]');
+
 document.querySelector("textarea").addEventListener("input", function(event) {
   const textArea = event.target.value;
 
   const callWordCount = analyzer.getWordCount(textArea);
-  document.querySelector('li[data-testid="word-count"]').innerHTML = `Recuento de palabras:<br>${callWordCount}`;
+  wordCounter.innerHTML = `Recuento de palabras:<br>${callWordCount}`;
 
   const callCharacterCount = analyzer.getCharacterCount(textArea);
-  document.querySelector('li[data-testid="character-count"]').innerHTML = `Recuento caracteres:<br>${callCharacterCount}`;
+  characterCounter.innerHTML = `Recuento caracteres:<br>${callCharacterCount}`;
 
   const callCharacterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(textArea);
-  document.querySelector('li[data-testid="character-no-spaces-count"]').innerHTML = `Recuento de caracteres sin espacios, ni signos:<br>${callCharacterCountExcludingSpaces}`;
+  characterNoSpacesCounter.innerHTML = `Recuento de caracteres sin espacios, ni signos:<br>${callCharacterCountExcludingSpaces}`;
 
   const callAvarageWordLength = analyzer.getAverageWordLength(textArea);
-  document.querySelector('li[data-testid="word-length-average"]').innerHTML = `Longitud promedio de palabras:<br>${callAvarageWordLength}`;
+  wordLengthAverageCounter.innerHTML = `Longitud promedio de palabras:<br>${callAvarageWordLength}`;
 
   const callNumberCount = analyzer.getNumberCount(textArea);
-  document.querySelector('li[data-testid="number-count"]').innerHTML = `Recuento de números:<br>${callNumberCount}`;
+  numberCounter.innerHTML = `Recuento de números:<br>${callNumberCount}`;
 
   const callNumberSum = analyzer.getNumberSum(textArea);
-  document.querySelector('li[data-testid="number-sum"]').innerHTML = `Suma total de números:<br>${callNumberSum}`;
+  numberSumCounter.innerHTML = `Suma total de números:<br>${callNumberSum}`;
 
 });
 
-
 const reload = document.getElementById("reset-button").addEventListener("click", function() {
-  location.reload();
+  document.querySelector("textarea").value = "";
+  wordCounter.innerText = "Recuento de palabras";
+  characterCounter.innerText = "Recuento caracteres";
+  characterNoSpacesCounter.innerText = "Recuento de caracteres sin espacios, ni signos";
+  wordLengthAverageCounter.innerText = "Longitud promedio de palabras";
+  numberCounter.innerText = "Recuento de números";
+  numberSumCounter.innerText = "Suma total de números";
+  // location.reload();
   reload;
 });
